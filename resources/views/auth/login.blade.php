6b3807ac0840fcb046a6771d1876b8cf89e0771d
behinIndustrialGroup <info@behin.energy>
2025-04-10 11:47:51 +0330

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,11 +46,9 @@
             <div class="center-align" style="text-align: center">
                 <a href="{{ route('password.request') }}" class="text-center">فراموشی رمز</a>
             </div> --}}
-                <a referrerpolicy='origin' target='_blank'
-                    href='https://trustseal.enamad.ir/?id=573760&Code=xxw9WMjEmu7Jif319UJnJYlBcrY3BgYa'><img
-                        referrerpolicy='origin'
-                        src='https://trustseal.enamad.ir/logo.aspx?id=573760&Code=xxw9WMjEmu7Jif319UJnJYlBcrY3BgYa'
-                        alt='' style='cursor:pointer' code='xxw9WMjEmu7Jif319UJnJYlBcrY3BgYa'></a>
+                <div class="col-sm-12 text-center">
+                    <a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=415037&Code=Ca929c8Fd94e346DE7ac52fb4D3ed935'><img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=415037&Code=Ca929c8Fd94e346DE7ac52fb4D3ed935' alt='' style='cursor:pointer' code='Ca929c8Fd94e346DE7ac52fb4D3ed935'></a>
+                </div>
                 <div class="col-sm-12 text-center p-0 m-0" style="font-size: 10px">
                     <p>نسخه {{ config('app.version') }}</p>
                 </div>
