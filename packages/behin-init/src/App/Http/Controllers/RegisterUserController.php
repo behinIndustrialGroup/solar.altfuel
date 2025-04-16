c05b937163f9c4eb750c30551331808133a7c9ae
behinIndustrialGroup <info@behin.energy>
2025-04-16 13:36:03 +0330

    Update RegisterUserController.php

--- packages/behin-init/src/App/Http/Controllers/RegisterUserController.php
+++ packages/behin-init/src/App/Http/Controllers/RegisterUserController.php
@@ -40,7 +40,7 @@ class RegisterUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role_id' => 2
+            'role_id' => 3
         ]);
 
         event(new Registered($user));
