c81f26757039320728ff32c93f934a066b951289
behinIndustrialGroup <info@behin.energy>
2025-05-14 19:00:26 +0330

    Update preview.blade.php

--- packages/behin-simple-workflow/src/Views/Core/Form/preview.blade.php
+++ packages/behin-simple-workflow/src/Views/Core/Form/preview.blade.php
@@ -38,7 +38,7 @@
                     {!! Form::help($fieldId, [
                         'options' => isset($fieldAttributes?->options) ? $fieldAttributes?->options : null,
                         'class' => '',
-                        'id' => $field->id ?? $fieldId,
+                        'id' => $fieldAttributes?->id ?? $fieldId,
                         'style' => isset($fieldAttributes?->style) ? $fieldAttributes?->style : null,
                         'script' => isset($fieldAttributes?->script) ? $fieldAttributes?->script : null,
                     ]) !!}
