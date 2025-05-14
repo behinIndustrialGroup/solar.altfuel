1693c9a0d4c8ef1c2f508dd5f324f5a2e24cfd9e
behinIndustrialGroup <info@behin.energy>
2025-05-14 18:53:42 +0330

    Update help.blade.php

--- packages/behin-simple-workflow/src/Views/Core/Field/help.blade.php
+++ packages/behin-simple-workflow/src/Views/Core/Field/help.blade.php
@@ -16,8 +16,23 @@
     'required' => false,
     'dir' => 'ltr'
 ]) !!}
-{!! Form::textarea('script', [
-    'value' => $attributes?->script ?? null,
-    'required' => false,
-    'dir' => 'ltr'
-]) !!}
+<div id="script-editor" style="height: 500px; width: 100%;font-size: 16px;">{{ $attributes?->script ?? null }}</div>
+<textarea name="script" id="script" dir="ltr" class="d-none">{{ $attributes?->script ?? null }}</textarea>
+<script>
+    const editor = ace.edit("script-editor");
+    editor.setTheme("ace/theme/monokai"); // انتخاب تم
+    editor.session.setMode("ace/mode/javascript"); // تنظیم زبان 
+
+
+
+    // غیرفعال کردن تحلیلگر پیش‌فرض Ace
+    editor.getSession().setUseWorker(false);
+
+    // فعال‌سازی خط‌بندی خودکار
+    editor.setOption("wrap", true);
+
+    // ذخیره محتوا به textarea مخفی
+    editor.session.on('change', function() {
+        $('#script').val(editor.getValue());
+    });
+</script>
