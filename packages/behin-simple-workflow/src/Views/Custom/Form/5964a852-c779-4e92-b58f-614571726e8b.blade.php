e874bedc3ece7e72936dafd7226533f11d0289e5
behinIndustrialGroup <info@behin.energy>
2025-05-18 09:36:56 +0330

    Update 5964a852-c779-4e92-b58f-614571726e8b.blade.php

--- packages/behin-simple-workflow/src/Views/Custom/Form/5964a852-c779-4e92-b58f-614571726e8b.blade.php
+++ packages/behin-simple-workflow/src/Views/Custom/Form/5964a852-c779-4e92-b58f-614571726e8b.blade.php
@@ -1,6 +1,24 @@
 @php
+    use App\Models\User;
     use Behin\SimpleWorkflow\Models\Entities\Users_profile;
+    use Behin\SimpleWorkflow\Models\Entities\Requests;
+    use Behin\SimpleWorkflow\Models\Entities\Panels;
+    use Behin\SimpleWorkflow\Models\Entities\Invertors;
+    use Behin\SimpleWorkflow\Models\Entities\Request_panels;
+    use Behin\SimpleWorkflow\Models\Entities\Request_invertors;
+    use Behin\SimpleWorkflow\Models\Entities\Powerhouse_place_info;
     $customerId = $case->getVariable('user_profile_id');
     $customer = Users_profile::find($customerId);
-    dd($customer);
+    $powerhousePlaceInfoId = $case->getVariable('powerhouse_place_info-id');
+    $powerhousePlaceInfo = Powerhouse_place_info::find($powerhousePlaceInfoId);
+    $contractorId = $case->getVariable('request-contractor_id');
+    $contractor = User::find($contractorId);
+    $technicianHeadId = $case->getVariable('request-technician_head_id');
+    $technicianHead = User::find($technicianHeadId);
+    $inspectorId = $case->getVariable('inspector');
+    $inspector = User::find($inspectorId);
+    $requestId = $case->getVariable('request-id');
+    $request = Requests::find($requestId);
+    $usedPanels = Request_panels::where('request_id', $requestId)->get();
+    $usedInvertors = Request_invertors::where('request_id', $requestId)->get();
 @endphp
