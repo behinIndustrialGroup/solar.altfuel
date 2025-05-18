558e05d68baaba70ede60a73b776fe648f0c9207
behinIndustrialGroup <info@behin.energy>
2025-05-18 09:24:32 +0330

    Update 5964a852-c779-4e92-b58f-614571726e8b.blade.php

--- packages/behin-simple-workflow/src/Views/Custom/Form/5964a852-c779-4e92-b58f-614571726e8b.blade.php
+++ packages/behin-simple-workflow/src/Views/Custom/Form/5964a852-c779-4e92-b58f-614571726e8b.blade.php
@@ -1,1 +1,6 @@
-test
+@php
+    use Behin\SimpleWorkflow\Models\Core\UserProfile;
+    $customerId = $case->getVariable('user_profile_id');
+    $customer = UserProfile::find($customerId);
+    dd($customer);
+@endphp
