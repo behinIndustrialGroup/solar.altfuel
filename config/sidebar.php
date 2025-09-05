d380403e84cd6494f66f870158b4ab767000d300
behinIndustrialGroup <info@behin.energy>
2025-09-05 15:46:45 +0330

    Update sidebar.php

--- config/sidebar.php
+++ config/sidebar.php
@@ -35,7 +35,7 @@ return [
         'workflow' => [
             'icon' => 'account_tree',
             'fa_name' => 'گردش کار',
-            'route-name' => '',
+            'route-name' => 'simpleWorkflow.process.index',
             'submenu' => [
                 'process' => [ 'fa_name' => 'فرایند', 'route-name' => 'simpleWorkflow.process.index' ],
                 'task-actors' => [ 'fa_name' => 'تسک ها', 'route-name' => 'simpleWorkflow.task-actors.index' ],
