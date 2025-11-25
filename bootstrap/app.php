6a34c5431afe682698643275154e844b29d6dd19
behinIndustrialGroup <info@behin.energy>
2025-11-25 09:23:51 +0330

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         // $middleware->append(Access::class);
         $middleware->append(Logging::class);
+        $middleware->alias([
+            'access' => Access::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
