d395c0419150b1d0a464ab3f9b16e589e074788e
behinIndustrialGroup <info@behin.energy>
2025-11-25 10:02:04 +0330

    Update contractor-show-request.blade.php

--- packages/solar-plant-requests/resources/views/requests/contractor-show-request.blade.php
+++ packages/solar-plant-requests/resources/views/requests/contractor-show-request.blade.php
@@ -18,7 +18,7 @@
                     @endif
 
                     <div class="card-header">
-                        <h5 class="mb-0">لیست درخواست‌ها</h5>
+                        <h5 class="mb-0">جزئیات درخواست</h5>
                     </div>
                     <div class="card-body table-responsive">
                         
