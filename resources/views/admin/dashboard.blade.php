03037b33201a98d295afb489e62a01228f100ea5
behinIndustrialGroup <info@behin.energy>
2025-11-26 12:47:59 +0330

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -41,6 +41,23 @@
                     </div>
                 </div>
             @endauth
+            @if (auth()->user()->role_id == 6)
+                <div class="col-sm-3 ">
+                    <!-- small box -->
+                    <div class="small-box bg-primary">
+                        <div class="inner">
+                            <h6>{{ trans('پنل') }}</h6>
+
+                            <p>{{ trans('افزودن پنل جدید') }}</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="{{ route('solar-plant-requests.panel.create') }}"
+                            class="small-box-footer">{{ trans('مشاهده') }} <i class="fa fa-arrow-circle-left"></i></a>
+                    </div>
+                </div>
+            @endauth
         </div>
     </div>
 @endsection
